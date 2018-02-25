1eab5ea08bbf6f69bf01bc98bb1056c99e8ff37a
Riley Westbrook <westbrookriley@gmail.com>
2018-02-25 17:51:52 -0500

    checkpoint 3 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 require 'pry'
+require 'json'
 
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
+  include JSON
 
   def initialize(email, password)
     response = self.class.post('/sessions', body: { "email": email, "password": password } )
@@ -13,5 +15,10 @@ class Kele
     else
       puts 'Welcome'
     end
+  end
+
+  def get_me
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: {"authorization" => @auth_token})
+    JSON.parse(response.body)
   end
 end
