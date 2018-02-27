3622fbbbc9152ba0aa3a346731138f5f389ca0cf
Riley Westbrook <westbrookriley@gmail.com>
2018-02-26 19:17:11 -0500

    checkpoint 4 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,4 +21,9 @@ class Kele
     response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: {"authorization" => @auth_token})
     JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
+    JSON.parse(response.body)
+  end
 end
