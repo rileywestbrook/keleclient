a55f461fc8c3592c17521f2ab2e128bada22e6bf
Riley Westbrook <westbrookriley@gmail.com>
2018-02-28 17:55:45 -0500

    checkpoint complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -57,4 +57,17 @@ class Kele
       puts "message sent!"
     end
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions",
+      body: {
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id,
+      },
+      headers: {"authorization" => @auth_token})
+    JSON.parse(response.body)
+  end
 end
